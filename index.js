c7e3ff6d2cad35f4b5b021f97b5f8f6e41027445
ezequielcalabresi-stack <ezequielcalabresi@gmail.com>
2026-08-30 17:37:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,9 @@ const client = new Client({
 
 const qrcode = require('qrcode');
 
-client.on('qr', async (qr) => {
+client.on('qr', (qr) => {
     console.log('Escanea este código QR desde el navegador:');
-    // Esto genera un link directo para ver el QR grande y clarito en cualquier navegador
-    console.log('https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + encodeURIComponent(qr));
+    console.log(`https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(qr)}`);
 });
 
 client.on('ready', () => {
